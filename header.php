055ff0194f7ead08b41004ee599b10780d2a6a6e
Joe Fallon <joseph.t.fallon@gmail.com>
2015-09-14 10:50:49 -0700

    A copy of http://clean-start-theme.com/ was added to the project as a starting place.

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+  <meta charset="<?php bloginfo( 'charset' ); ?>" />
+  <title>
+    <?php
+      if( ! is_home() ):
+        wp_title( '|', true, 'right' );
+      endif;
+      bloginfo( 'name' );
+    ?>
+  </title>
+  <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+<?php wp_nav_menu( array('theme_location' => 'primary') ); ?>
